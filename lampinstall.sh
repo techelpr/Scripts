4ea825da63da45ffbf185c305428030f574bbd3f
techelpr <techelpr@gmail.com>
2018-01-10 14:58:58 -0500

    Update lampinstall.sh
    Added PHP plugins.

--- lampinstall.sh
+++ lampinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 
-sudo apt install apache2 mysql-server php php7.0 libapache2-mod-php libapache2-mod-php7.0 php-mcrypt php7.0-mcrypt php-mysql php7.0-mysql php-cgi php7.0-cgi php-curl php7.0-curl apache2-data apache2-utils libevent-core-2.0-5 mysql-client-5.7 mysql-client-core-5.7 mysql-server-5.7 mysql-server-core-5.7
+sudo apt install apache2 mysql-server php php7.0 php-mcrypt php7.0-mcrypt php-mysql php7.0-mysql php-cgi php7.0-cgi php-curl php7.0-curl php-xml php7.0-xml php-mbstring php7.0-mbstring libapache2-mod-php libapache2-mod-php7.0 apache2-data apache2-utils libevent-core-2.0-5 mysql-client-5.7 mysql-client-core-5.7 mysql-server-5.7 mysql-server-core-5.7
 
 sudo systemctl restart apache2
 sudo systemctl status apache2
